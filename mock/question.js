2a43e7c57eb0da2242873795ef5507da671c8a8c
CQY <coolfish@xinyue.io>
2023-05-22 18:42:55 +0800

    feat: 完成问卷列表接口相关功能

--- mock/question.js
+++ mock/question.js
@@ -1,12 +1,12 @@
-const Mock = require("mockjs");
-const getQuestionList = require("./data/getQuestionList.js");
+const Mock = require('mockjs');
+const getQuestionList = require('./data/getQuestionList.js');
 
 const Random = Mock.Random;
 
 module.exports = [
 	{
-		url: "/api/question/:id",
-		method: "get",
+		url: '/api/question/:id',
+		method: 'get',
 		response() {
 			return {
 				errno: 0,
@@ -18,8 +18,8 @@ module.exports = [
 		},
 	},
 	{
-		url: "/api/question",
-		method: "post",
+		url: '/api/question',
+		method: 'post',
 		response() {
 			return {
 				errno: 0,
@@ -31,16 +31,18 @@ module.exports = [
 	},
 	{
 		// 获取(查询)问卷列表
-		url: "/api/question",
-		method: "get",
+		url: '/api/question',
+		method: 'get',
 		response(ctx) {
-			const { url = "" } = ctx;
-			const isDeleted = url.includes("isDeleted=true");
-			const isStar = url.includes("isStar=true");
+			const { url = '', query = {} } = ctx;
+			const isDeleted = url.includes('isDeleted=true');
+			const isStar = url.includes('isStar=true');
+			const pageSize = Number(query.pageSize);
+
 			return {
 				errno: 0,
 				data: {
-					list: getQuestionList({ isDeleted, isStar }),
+					list: getQuestionList({ len: pageSize, isDeleted, isStar }),
 					total: 100,
 				},
 			};
@@ -48,8 +50,31 @@ module.exports = [
 	},
 	{
 		// 更新问卷
-		url: "/api/question/:id",
-		method: "patch",
+		url: '/api/question/:id',
+		method: 'patch',
+		response() {
+			return {
+				errno: 0,
+			};
+		},
+	},
+	{
+		// 复制问卷
+		url: '/api/question/duplicate/:id',
+		method: 'post',
+		response() {
+			return {
+				errno: 0,
+				data: {
+					id: Random.id(),
+				},
+			};
+		},
+	},
+	{
+		// 删除问卷
+		url: '/api/question',
+		method: 'delete',
 		response() {
 			return {
 				errno: 0,
